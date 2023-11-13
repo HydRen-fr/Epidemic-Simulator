1de173d40b2fed92f1389f0543c8ea0aaff616ac
Helios <130355604+HydRen-fr@users.noreply.github.com>
2023-11-13 13:01:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ LARGEUR_ECRAN = 800
 HAUTEUR_ECRAN = 600
 COULEUR_MUR = (0, 0, 0)  # Noir
 COULEUR_FOND = (255, 255, 255)  # Blanc
-TAILLE_CARRE = 230
-TAILLE_INDIVIDU = 5
-NB_INDIVIDUS = 50
+TAILLE_CARRE = 450
+TAILLE_INDIVIDU = 4
+NB_INDIVIDUS = 70
+VITESSE_MAX = 0.4
 
-# Initialisation des modules pygame manuellement
-# pygame.init() fait tout d'un coup et peut créer des bugs
+# Initialisation des modules Pygame manuellement
 pygame.display.init()
 pygame.font.init()
 
@@ -26,17 +26,38 @@ class Individu:
         self.y = y
         self.taille = taille
         self.couleur = couleur
+        self.vitesse_x = random.uniform(-VITESSE_MAX, VITESSE_MAX)
+        self.vitesse_y = random.uniform(-VITESSE_MAX, VITESSE_MAX)
 
     def dessiner(self):
         pygame.draw.circle(ecran, self.couleur, (int(self.x), int(self.y)), self.taille)
 
+    def bouger(self):
+        self.x += self.vitesse_x
+        self.y += self.vitesse_y
+
+    def rebondir_sur_murs(self):
+        if self.x - self.taille < (LARGEUR_ECRAN - TAILLE_CARRE) // 2 or self.x + self.taille > (LARGEUR_ECRAN + TAILLE_CARRE) // 2:
+            self.vitesse_x = -self.vitesse_x
+        if self.y - self.taille < (HAUTEUR_ECRAN - TAILLE_CARRE) // 2 or self.y + self.taille > (HAUTEUR_ECRAN + TAILLE_CARRE) // 2:
+            self.vitesse_y = -self.vitesse_y
+
+    def detecter_collision(self, autre_individu):
+        distance = ((self.x - autre_individu.x) ** 2 + (self.y - autre_individu.y) ** 2) ** 0.5
+        return distance <= self.taille + autre_individu.taille
+
 # Création d'une liste d'Individus
 individus = [Individu(
-    random.randint((LARGEUR_ECRAN - TAILLE_CARRE) // 2, (LARGEUR_ECRAN + TAILLE_CARRE) // 2),
-    random.randint((HAUTEUR_ECRAN - TAILLE_CARRE) // 2, (HAUTEUR_ECRAN + TAILLE_CARRE) // 2),
+    random.randint((LARGEUR_ECRAN - TAILLE_CARRE) // 2 + TAILLE_INDIVIDU, (LARGEUR_ECRAN + TAILLE_CARRE) // 2 - TAILLE_INDIVIDU),
+    random.randint((HAUTEUR_ECRAN - TAILLE_CARRE) // 2 + TAILLE_INDIVIDU, (HAUTEUR_ECRAN + TAILLE_CARRE) // 2 - TAILLE_INDIVIDU),
     TAILLE_INDIVIDU,
-    (0, 120, 240)
-) for _ in range(NB_INDIVIDUS)]
+    (255, 0, 0)  # Rouge pour le premier individu
+) for _ in range(1)] + [Individu(
+    random.randint((LARGEUR_ECRAN - TAILLE_CARRE) // 2 + TAILLE_INDIVIDU, (LARGEUR_ECRAN + TAILLE_CARRE) // 2 - TAILLE_INDIVIDU),
+    random.randint((HAUTEUR_ECRAN - TAILLE_CARRE) // 2 + TAILLE_INDIVIDU, (HAUTEUR_ECRAN + TAILLE_CARRE) // 2 - TAILLE_INDIVIDU),
+    TAILLE_INDIVIDU,
+    (0, 0, 255)  # Bleu pour le deuxième individu
+) for _ in range(NB_INDIVIDUS - 1)]
 
 # Boucle principale
 while True:
@@ -51,10 +72,17 @@ while True:
     # Dessiner le carré noir
     pygame.draw.rect(ecran, COULEUR_MUR, pygame.Rect((LARGEUR_ECRAN - TAILLE_CARRE) // 2, (HAUTEUR_ECRAN - TAILLE_CARRE) // 2, TAILLE_CARRE, TAILLE_CARRE), 2)
 
-    # Dessiner les Individus
-    for ind in individus:
-        ind.dessiner()
+    # Déplacer et faire rebondir les Individus
+    for individu in individus:
+        individu.bouger()
+        individu.rebondir_sur_murs()
+        individu.dessiner()
+
+    # Tester les collisions et mettre à jour les couleurs
+    for i in range(len(individus)):
+        for j in range(len(individus)):
+            if i != j and individus[i].detecter_collision(individus[j]):
+                individus[i].couleur, individus[j].couleur = (255, 0, 0), (255, 0, 0)  # Rouges tous les deux
 
     # Mettre à jour l'affichage
     pygame.display.flip()
-
