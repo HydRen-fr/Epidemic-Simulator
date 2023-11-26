c51c97ad04d750982c421b9447a3ecb13d3a1224
Helios <130355604+HydRen-fr@users.noreply.github.com>
2023-11-26 17:11:29 +0100

    Add files via upload

---
+++ v8/main.py
@@ -0,0 +1,10 @@
+# Programme principal
+
+from classes import Cosmos
+
+# Création d'une instance de la classe Cosmos pour gérer la simulation
+cosmos = Cosmos()
+
+# Boucle principale de la simulation
+while True:
+    cosmos.simulation()
