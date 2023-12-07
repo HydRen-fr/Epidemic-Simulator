efce9f58dae7f2d9c5dd01311265832f2b0ed71a
Helios <130355604+HydRen-fr@users.noreply.github.com>
2023-12-07 13:41:47 +0100

    Add files via upload

---
+++ v9/lancer_interface.py
@@ -0,0 +1,163 @@
+import pygame
+import pygame_gui
+from pygame_gui.elements import UIHorizontalSlider, UIButton, UILabel
+from constantes import LARGEUR_ECRAN, HAUTEUR_ECRAN
+import le_cosmos
+
+
+# Définition des paramètres par défaut en attendant une modification
+# Via l'interface utilisateur
+parametres_de_base = {
+        "NB_INDIVIDUS": 135,  # Nombre total d'individus dans la simulation
+        "VITESSE_MAX": 0.15,  # Vitesse maximale des individus. Influence la propagation.
+        "TAUX_INFECTIOSITE": 0.15,  # La probabilité d'être infecté. Entre 0 et 1.
+        "TAUX_ASYMPTOMATIQUES": 0.55,  # La probabilité d'être asymptomatique et jamais envoyé en quarantaine. Entre 0 et 1.
+        "LETALITE": 0.2,  # Chances de mourir du virus
+        "LA_QUARANTAINE": True,  # Quarantaine ou pas
+        "RAYON_DISTANCIATION": False,  # Distanciation sociale ou pas (difficile de le faire modulable)
+        "TAUX_DISSIDENTS": 0.2,  # La probabilité d'être un individu qui n'écoute pas les consignes de distanciation
+        # Modes de simulation
+        "VILLE_CENTRALE": False,  # Activer le mode "Ville centrale"
+        "COMMUNAUTES": False,  # Activer le mode "Communautés"
+        "PROBABILITE_VOYAGE_VERS_VILLE": 0.9,  # Probabilité de voyage vers la ville
+        # Doit être très basse car les tours de boucle s'enchaînent très vite --> 10**-2
+        "PROBABILITE_VOYAGE_VERS_COMMUNAUTE": 0.6,  # Probabilité de voyage vers une autre communauté
+        # Doit être très basse car les tours de boucle s'enchaînent très vite --> 10**-3
+        }
+
+
+class Lancement:
+    def __init__(self, params):
+        # Création d'une instance de la classe Cosmos pour gérer la simulation
+        self.cosmos = le_cosmos.Cosmos(params)
+        self.cosmos.simulation()
+
+
+class Parametres:
+    def __init__(self, derniers_params_utilises):
+        pygame.init()
+
+        # Police pour écrire
+        self.font = pygame.font.Font(None, 60)
+
+        self.ui_manager = pygame_gui.UIManager((LARGEUR_ECRAN, HAUTEUR_ECRAN))
+        self.ui_manager.get_theme().load_theme('v9/horizontal_slider.json')
+        self.ui_manager.get_theme().load_theme('v9/slider_label.json')
+        
+
+        self.label_mini_maxi = [
+            ("Nombre d'individus", 1, 270),
+            ("Vitesse maximale", 0.1, 1),
+            ("Taux d'infectiosité", 0.1, 1),
+            ("Taux d'asymptomatiques", 0.1, 1),
+            ("Létalité", 0.1, 1),
+            ("Quarantaine", None, None),
+            ("Distanciation", None, None),
+            ("Taux de dissidents", 0.1, 1),
+            ("Ville centrale", None, None),
+            ("Communautés", None, None),
+            ("Probabilité de voyage vers la ville", 0, 1),
+            ("Probabilité de voyage vers une communauté", 0, 1)
+        ]
+
+        self.constantes_liste = [
+            (param, label, mini, maxi, derniers_params_utilises[param]) \
+            for param, (label, mini, maxi) in zip(derniers_params_utilises.keys(), self.label_mini_maxi)
+        ]
+
+        # Dictionnaire pour stocker les valeurs du moment avec leur nom de variable
+        self.nvar_current_values = {nvar: default_val for (nvar, _, _, _, default_val) in self.constantes_liste}
+
+        # Chargement de l'image de la flèche retour et redimensionnement
+        original_arrow_image = pygame.image.load("v9/imgs/go_back_arrow.png")
+        new_width = 100
+        new_height = 100
+        self.arrow_image = pygame.transform.scale(original_arrow_image, (new_width, new_height))
+
+        # Utilisation des constantes pour placer l'image en haut à droite
+        self.arrow_rect = self.arrow_image.get_rect()
+        self.arrow_rect.topright = (130, 10)
+
+        self.create_ui()
+
+    def create_ui(self):
+        self.y_offset = 120
+        self.elements = []
+
+        for index, (nvar, label, min_val, max_val, default_val) in enumerate(self.constantes_liste):
+            label_text = f"{label}: {nvar}"
+
+            if isinstance(default_val, bool):
+                button_rect = pygame.Rect((380, self.y_offset), (350, 30))
+                button_value = default_val
+                button = UIButton(button_rect, f"{label}: {'OUI' if button_value else 'NON'}", self.ui_manager, None, object_id=f'#bool_button_{index}')
+                self.elements.append((button, None, index, label, nvar))
+            else:
+                slider_rect = pygame.Rect((380, self.y_offset), (300, 30))
+                slider = UIHorizontalSlider(slider_rect, default_val, (min_val, max_val), self.ui_manager, None, object_id='#horizontal_slider')
+                slider.set_current_value(default_val)
+
+                value_label_rect = pygame.Rect((630, self.y_offset), (770, 30))
+                value_label = UILabel(value_label_rect, label_text, self.ui_manager, None, object_id='#slider_label')
+                self.elements.append((slider, value_label, index, label, nvar))
+
+            self.y_offset += 60
+
+
+    def update_ui(self):
+        for element_data in self.elements:
+            element = element_data[0]
+            label = element_data[3]
+            nvar = element_data[4]
+            value_label = element_data[1]  # Que pour UIHorizontalSlider
+            
+            if isinstance(element, UIButton):
+                if element.check_pressed():
+                    # Inverser le booléen
+                    self.nvar_current_values[nvar] = not self.nvar_current_values[nvar]
+                    element.set_text(f"{label}: {'OUI' if self.nvar_current_values[nvar] else 'NON'}")
+            elif isinstance(element, UIHorizontalSlider):
+                self.nvar_current_values[nvar] = round(element.get_current_value(), 2)
+                value_label.set_text(f"{label}: {self.nvar_current_values[nvar]}")
+
+
+    def run(self):
+        clock = pygame.time.Clock()
+        is_running = True
+
+        while is_running:
+            time_delta = clock.tick(60) / 1000.0
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_running = False
+
+                self.ui_manager.process_events(event)
+
+                # Vérifier si le clic de souris a eu lieu sur l'image de flèche
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if self.arrow_rect.collidepoint(event.pos):
+                        # Relancer la simulation avec de nouveaux paramètres
+                        Lancement(self.nvar_current_values)
+
+
+            self.ui_manager.update(time_delta)
+            self.update_ui()
+
+            # Écran blanc
+            le_cosmos.ecran.fill((255, 255, 255))
+            # Dessiner la flèche retour
+            le_cosmos.ecran.blit(self.arrow_image, self.arrow_rect)
+            # Titre explicite
+            titre = self.font.render("Paramètres de la simulation", True, (0, 0, 0))
+            le_cosmos.ecran.blit(titre, (LARGEUR_ECRAN//2 - 255, 30))
+            # Le reste
+            self.ui_manager.draw_ui(le_cosmos.ecran)
+
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    Lancement(parametres_de_base)
---
+++ v9/le_cosmos.py
@@ -0,0 +1,217 @@
+# Importation des modules nécessaires
+import pygame  # Bibliothèque pour la création de jeux-vidéos
+import random  # Module pour la génération de nombres aléatoires
+import sys  # Module fournissant un accès à certaines variables utilisées ou maintenues par l'interprétateur python
+
+from constantes import * # On importe les variables de constantes.py
+
+import modes
+import les_individus
+import prevention
+import stats
+
+import lancer_interface
+from pygame.locals import MOUSEBUTTONDOWN # Checker les clics
+
+
+# Initialisation du module pygame display manuellement. Peut éviter des bugs.
+pygame.display.init()
+
+# Création de la fenêtre de simulation
+ecran = pygame.display.set_mode((LARGEUR_ECRAN, HAUTEUR_ECRAN))
+pygame.display.set_caption("Epydemie")
+
+
+class Cosmos:
+    def __init__(self, les_parametres):
+        # On stocke en attribut les paramètres qui seront les constantes de la simulation
+        # dict() évite une erreur mémoire
+        self.constantes_parametrables = dict(les_parametres)
+        # Ajustements pour la simulation
+        self.constantes_parametrables["PROBABILITE_VOYAGE_VERS_VILLE"] *= 10**-2
+        # Doit être très basse car les tours de boucle s'enchaînent très vite --> 10**-2
+        self.constantes_parametrables["PROBABILITE_VOYAGE_VERS_COMMUNAUTE"] *= 10**-3
+        # Encore plus ici --> 10**-3
+
+        # Paramètres changés ou pas - condition while
+        self.pas_de_changements = True
+        
+        # Si demandé on active la grille des communautés
+        if self.constantes_parametrables["COMMUNAUTES"]:
+            self.nb_communautes_x = TAILLE_CARRE // TAILLE_COMMUNAUTE
+            self.nb_communautes_y = TAILLE_CARRE // TAILLE_COMMUNAUTE
+            self.communautes = modes.Communautes.initialiser_communautes(self)
+
+        # Liste des individus qui prendront part à la simulation
+        self.individus = self.initialiser_individus()
+
+        # Liste des couples d'individus en collision
+        self.collisions_en_cours = []
+
+        # Compteur des individus morts
+        self.nb_morts = 0
+
+        # Chargement de l'image du bouton et redimensionnement
+        original_button_image = pygame.image.load("v9/imgs/parametres.png")
+        new_width = 145
+        new_height = 145
+        self.button_image = pygame.transform.scale(original_button_image, (new_width, new_height))
+
+        # Utilisation des constantes pour placer l'image en haut à droite
+        self.button_rect = self.button_image.get_rect()
+        self.button_rect.topright = (LARGEUR_ECRAN-10, 10)
+
+
+        self.stats = stats.Stats()
+        self.parametres = lancer_interface.Parametres(les_parametres)
+
+
+    def initialiser_individus(self):
+        # Création d'une liste d'individus pour la simulation
+        individus = []
+
+        if self.constantes_parametrables["COMMUNAUTES"]:
+            for i in range(self.constantes_parametrables["NB_INDIVIDUS"]):
+                communaute_x, communaute_y = modes.Communautes.choisir_communaute(self)
+                x = random.uniform(communaute_x, communaute_x + TAILLE_COMMUNAUTE - TAILLE_INDIVIDU)
+                y = random.uniform(communaute_y, communaute_y + TAILLE_COMMUNAUTE - TAILLE_INDIVIDU)
+                couleur = (0, 0, 255)  # Bleu par défaut
+                if i == 0:
+                    couleur = (255, 0, 0)  # Rouge pour le premier individu infecté
+                individus.append(les_individus.Individu(x, y, TAILLE_INDIVIDU, couleur, self))
+
+
+            
+        else:
+            individus = [
+                les_individus.Individu(
+                    random.randint(EMPLACEMENT_CARRE_X + TAILLE_INDIVIDU, EMPLACEMENT_CARRE_X_CONJ - TAILLE_INDIVIDU),
+                    random.randint(EMPLACEMENT_CARRE_Y + TAILLE_INDIVIDU, EMPLACEMENT_CARRE_Y_CONJ - TAILLE_INDIVIDU),
+                    TAILLE_INDIVIDU,
+                    (255, 0, 0),  # Rouge pour le premier individu infecté
+                    self,
+                )
+            ] + [
+                les_individus.Individu(
+                    random.randint(EMPLACEMENT_CARRE_X + TAILLE_INDIVIDU, EMPLACEMENT_CARRE_X_CONJ - TAILLE_INDIVIDU),
+                    random.randint(EMPLACEMENT_CARRE_Y + TAILLE_INDIVIDU, EMPLACEMENT_CARRE_Y_CONJ - TAILLE_INDIVIDU),
+                    TAILLE_INDIVIDU,
+                    (0, 0, 255),  # Bleu par défaut
+                    self,
+                ) for _ in range(self.constantes_parametrables["NB_INDIVIDUS"] - 1)
+            ]
+        
+        return individus        
+
+
+    def simulation(self):
+        # Boucle principale de la simulation
+        while self.pas_de_changements:
+
+            # Permet de quitter sans problème en fermant la fenêtre
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                elif event.type == MOUSEBUTTONDOWN:
+                    # Vérifier si le clic de souris a eu lieu sur le bouton
+                    if self.button_rect.collidepoint(event.pos):
+                        # On ouvre les paramètres
+                        self.parametres.run()
+                        self.pas_de_changements = False
+                        # Une fois l'interface fermée ça repart avec les modifs
+
+            
+            # DESSINS
+
+            # Dessiner le fond blanc
+            ecran.fill(COULEUR_FOND)
+
+            # Dessiner le bouton
+            ecran.blit(self.button_image, self.button_rect.topleft)
+
+            if self.constantes_parametrables["LA_QUARANTAINE"]:
+                # Dessiner le cadre de la quarantaine
+                pygame.draw.rect(ecran, COULEUR_MUR, pygame.Rect(EMPLACEMENT_QUARANTAINE_X, EMPLACEMENT_QUARANTAINE_Y, TAILLE_QUARANTAINE, TAILLE_QUARANTAINE), 2)
+            if self.constantes_parametrables["COMMUNAUTES"]:
+                # Dessiner les communautés
+                for communaute_x, communaute_y in self.communautes:
+                    pygame.draw.rect(ecran, COULEUR_MUR, pygame.Rect(communaute_x, communaute_y, TAILLE_COMMUNAUTE, TAILLE_COMMUNAUTE), 2)
+            else:
+                # Dessiner le cadre noir
+                pygame.draw.rect(ecran, COULEUR_MUR, pygame.Rect(EMPLACEMENT_CARRE_X, EMPLACEMENT_CARRE_Y, TAILLE_CARRE, TAILLE_CARRE), 2)
+                if self.constantes_parametrables["VILLE_CENTRALE"]:
+                    # Dessiner la ville
+                    pygame.draw.rect(ecran, (255, 165, 0), pygame.Rect(EMPLACEMENT_VILLE_X, EMPLACEMENT_VILLE_Y, TAILLE_VILLE, TAILLE_VILLE), 2)
+
+
+
+            # ACTUALISATION DES VIVANTS ET MORTS + DISTANCIATION
+                    
+            # Filtrer les individus en vie
+            self.individus = [ind for ind in self.individus if ind.vie]
+            # Actualiser le nombre de morts
+            self.nb_morts = self.constantes_parametrables["NB_INDIVIDUS"] - len(self.individus)
+
+            # Appliquer la distanciation sociale si c'est demandé
+            if self.constantes_parametrables["RAYON_DISTANCIATION"]:
+                for individu in self.individus:
+                    if not individu.est_en_quarantaine:
+                        individu.appliquer_distanciation(self.individus)
+
+
+
+            # ROUTINE
+            # self en argument = donner accès aux attributs de Cosmos
+            # Gérer le statut de chaque individu + déplacer et faire rebondir les individus dans le cadre
+            for individu in self.individus:
+                prevention.Guerison.progresser_guerison_ou_mort(individu, self)
+                if self.constantes_parametrables["LA_QUARANTAINE"]:
+                    prevention.Quarantaine.placer_en_quarantaine(individu)
+                    prevention.Quarantaine.gestion_quarantaine(individu, self)
+                individu.bouger(self)
+                individu.rebondir_sur_murs(self)
+                individu.dessiner()
+
+
+            
+            # COLLISIONS
+
+            # Vérifier les collisions en cours
+            self.collisions_en_cours = [(i, j) for (i, j) in self.collisions_en_cours if i.detecter_collision(j)]
+
+            # Tester les collisions et mettre à jour les couleurs
+            ind_no_quarantaine = [ind for ind in self.individus if not ind.est_en_quarantaine]
+            for i in range(len(ind_no_quarantaine)):
+                for j in range(i + 1, len(ind_no_quarantaine)):
+                    if (
+                        (ind_no_quarantaine[i], ind_no_quarantaine[j]) not in self.collisions_en_cours
+                        and ind_no_quarantaine[i].detecter_collision(ind_no_quarantaine[j])
+                    ):
+                        self.collisions_en_cours.append((ind_no_quarantaine[i], ind_no_quarantaine[j]))
+                        # L'un des deux est rouge
+                        if ind_no_quarantaine[i].couleur == (255, 0, 0) or ind_no_quarantaine[j].couleur == (255, 0, 0):
+                            # Utiliser random.random() pour générer un nombre entre 0 et 1
+                            if random.random() < self.constantes_parametrables["TAUX_INFECTIOSITE"]:
+                                # Si ça passe en-dessous du taux d'infectiosité on infecte
+                                ind_no_quarantaine[i].infecter(), ind_no_quarantaine[j].infecter()
+
+
+
+
+            # STATS
+
+            # Compteurs pour les statistiques
+            nb_bleus = sum(1 for ind in self.individus if ind.couleur == (0, 0, 255))
+            nb_rouges = sum(1 for ind in self.individus if ind.couleur == (255, 0, 0))
+            nb_verts = sum(1 for ind in self.individus if ind.couleur == (0, 255, 0))
+            # nb_morts initialisé plus tôt et en attribut de la classe comme c'est linéaire
+
+            self.stats.afficher_statistiques(nb_bleus, nb_rouges, nb_verts, self.nb_morts)
+            self.stats.actualiser_courbes(nb_bleus, nb_rouges, nb_verts, self.nb_morts, self)
+            self.stats.afficher_courbes()
+
+
+            # MAJ AFFICHAGE
+            pygame.display.flip()
